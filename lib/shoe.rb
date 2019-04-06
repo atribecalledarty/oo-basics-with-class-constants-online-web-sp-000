596e791af4083319c9a8c8651c81455dc200b14d
Arthur Song <arth3rs0ng@gmail.com>
2019-04-06 01:19:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,8 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand= (brand)
-    @brand = brand
+ # def brand= (brand)
+ #   @brand = brand
     BRANDS << brand if !BRANDS.include?(brand)
   end
 end
