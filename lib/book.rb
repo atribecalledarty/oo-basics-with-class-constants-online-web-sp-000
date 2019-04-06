80455538eaa81bfa6957095f58eb1b2c4c0f0c4d
Arthur Song <arth3rs0ng@gmail.com>
2019-04-06 01:16:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,7 +7,11 @@ class Book
   def initialize(title)
     @title = title
   end
-
+  
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
+  end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
